552cd7f04abce76d3809975671bcf060333620d1
cooknat <natalie.t.cook@gmail.com>
2018-11-22 21:37:25 +0000

    create club model

---
+++ db/migrate/20181122213350_create_clubs.rb
@@ -0,0 +1,13 @@
+class CreateClubs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :clubs do |t|
+      t.string :name
+      t.string :address
+      t.string :contact
+      t.string :email
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
