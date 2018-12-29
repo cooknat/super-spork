3a9bb59f1bccba305f6de2df3d9ec5860d947b89
cooknat <natalie.t.cook@gmail.com>
2018-12-29 21:49:23 +0000

    show

--- spec/request/clubs_spec.rb
+++ spec/request/clubs_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe ClubsController, type: :request do
       expect(response).to render_template(:index)
     end
 	end
+
+	describe '#show' do
+		it "returns http success" do
+      get '/clubs'
+      expect(response.status).to eq 200
+    end
+    
+    it "renders the show template" do
+      get '/clubs'
+      expect(response).to render_template(:show)
+    end
+	end
 end
