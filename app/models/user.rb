efd9309f118a42a171f06c38858609bcc8f8a404
cooknat <natalie.t.cook@gmail.com>
2019-03-21 22:21:29 +0000

    add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+
+	before_save { self.role ||= :member }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
